2368ebbe50e6cce9b43fef730b56a26221b38d22
Sang Nguyen Lu Hoang Sang <intern.nlhsang@tma.com.vn>
2022-12-16 09:19:02 +0700

    initial client

---
+++ client/src/app/store.js
@@ -0,0 +1,39 @@
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
+import {
+  FLUSH,
+  PAUSE,
+  PERSIST,
+  persistReducer,
+  persistStore,
+  PURGE,
+  REGISTER,
+  REHYDRATE,
+} from "redux-persist";
+import storage from "redux-persist/lib/storage";
+
+const persistConfig = {
+  key: "root",
+  storage,
+};
+const rootReducer = combineReducers({});
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+export const store = configureStore({
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoreActions: [
+          FLUSH,
+          PAUSE,
+          PERSIST,
+          persistStore,
+          PURGE,
+          REGISTER,
+          REHYDRATE,
+        ],
+      },
+    }),
+});
+
+export const persistor = persistStore(store);
