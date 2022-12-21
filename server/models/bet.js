d71ec6d2abbf782c8bb7b25785f209543a03d43b
Nguyen Lu Hoang Sang <hoangsang.nguyen2001a@gmail.com>
2022-12-21 17:00:33 +0700

    update bet model

--- server/models/bet.js
+++ server/models/bet.js
@@ -2,15 +2,24 @@ import { model, Schema } from "mongoose";
 
 const betSchema = new Schema(
   {
-    option: [
-      {
-        type: Schema.Types.ObjectId,
-        ref: "Team",
-      },
-    ],
-    money: {
+    team: {
+      type: Schema.Types.ObjectId,
+      ref: "Team",
+      require: true,
+    },
+    user: {
       type: Schema.Types.ObjectId,
       ref: "User",
+      require: true,
+    },
+    match: {
+      type: Schema.Types.ObjectId,
+      ref: "Match",
+      require: true,
+    },
+    money: {
+      type: Number,
+      default: 50,
     },
   },
   { timestamps: true }
