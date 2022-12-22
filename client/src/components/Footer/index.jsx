286500dabc45321990261f291d211b7b0c3cd3a9
Nguyen Lu Hoang Sang <hoangsang.nguyen2001a@gmail.com>
2022-12-22 15:39:19 +0700

    create footer component

--- client/src/components/Footer/index.jsx
+++ client/src/components/Footer/index.jsx
@@ -1,7 +1,14 @@
 import React from "react";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <footer className="h-[50px] uppercase bg-black text-white flex items-center">
+      <div className="max-w-7xl mx-auto uppercase font-semibold">
+        © wibet 2022 by{" "}
+        <span className="text-[#067CCD] cursor-pointer">wsi-dev</span>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
