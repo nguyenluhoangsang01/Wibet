a817dc10bc631cb943b275f08a168db5eef22946
Nguyen Lu Hoang Sang <hoangsang.nguyen2001a@gmail.com>
2022-12-23 11:35:50 +0700

    divide perm page

--- client/src/views/Matches/index.jsx
+++ client/src/views/Matches/index.jsx
@@ -1,15 +1,25 @@
 import React from "react";
-import { useLocation } from "react-router-dom";
+import { useSelector } from "react-redux";
+import { Navigate, useLocation } from "react-router-dom";
 import Breadcrumbs from "../../components/Breadcrumbs";
 import Heading from "../../components/Heading";
 import { matchesRoutes } from "../../constants";
+import { selectUser } from "../../state/userSlice";
 
 const Matches = () => {
+  // Get pathname from location
   const { pathname } = useLocation();
+  // Get user form global state
+  const { user } = useSelector(selectUser);
+
+  // Check if user is null
+  if (!user) return <Navigate to="/" />;
 
   return (
     <div>
+      {/* Breadcrumbs */}
       <Breadcrumbs routes={matchesRoutes} />
+      {/* Heading */}
       <Heading title={pathname.slice(1)} />
     </div>
   );
--- client/src/views/Team/index.jsx
+++ client/src/views/Team/index.jsx
@@ -1,15 +1,25 @@
 import React from "react";
-import { useLocation } from "react-router-dom";
+import { useSelector } from "react-redux";
+import { Navigate, useLocation } from "react-router-dom";
 import Breadcrumbs from "../../components/Breadcrumbs";
 import Heading from "../../components/Heading";
 import { teamRoutes } from "../../constants";
+import { selectUser } from "../../state/userSlice";
 
 const Team = () => {
+  // Get pathname from location
   const { pathname } = useLocation();
+  // Get user form global state
+  const { user } = useSelector(selectUser);
+
+  // Check if user role ID is difference Admin back to home page
+  if (user?.roleID !== "Admin") return <Navigate to="/" />;
 
   return (
     <div>
+      {/* Breadcrumbs */}
       <Breadcrumbs routes={teamRoutes} />
+      {/* Heading */}
       <Heading title={pathname.slice(1)} />
     </div>
   );
