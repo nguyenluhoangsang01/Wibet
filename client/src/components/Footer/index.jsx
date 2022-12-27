a3658a870de5f53b5131719981d9f63602d2d35b
Nguyen Lu Hoang Sang <hoangsang.nguyen2001a@gmail.com>
2022-12-27 15:23:04 +0700

    update styles for footer

--- client/src/components/Footer/index.jsx
+++ client/src/components/Footer/index.jsx
@@ -2,10 +2,10 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <footer className="h-[60px] uppercase bg-[#f5f5f5] border-t-[1px] border-t-[#ddd] flex items-center">
+    <footer className="h-[60px] uppercase bg-[#f5f5f5] border-t-[1px] border-t-[#ddd] flex items-center text-sm">
       <div className="uppercase font-semibold transition px-4 sm:px-10 mx-auto md:mx-0">
         © wibet 2022 by{" "}
-        <span className="text-[#067CCD] cursor-pointer">wsi-dev</span>
+        <span className="text-[#428BCA] cursor-pointer">wsi-dev</span>
       </div>
     </footer>
   );
