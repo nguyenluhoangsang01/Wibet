3d51cfaf0c8e5593dce3e7532c051adb7a3f9fc8
Nguyen Lu Hoang Sang <hoangsang.nguyen2001a@gmail.com>
2023-01-09 10:38:37 +0700

    fix footer

--- client/src/components/Footer/index.jsx
+++ client/src/components/Footer/index.jsx
@@ -1,11 +1,17 @@
 import React from "react";
+import { Link, useLocation } from "react-router-dom";
 
 const Footer = () => {
+  const { pathname } = useLocation();
+  console.log(pathname);
+
   return (
     <footer className="h-[60px] bg-[#f5f5f5] border-t-[1px] border-t-[#ddd] flex items-center text-[16px]">
       <p className="px-4 sm:px-10 mx-auto md:mx-0">
         © Wibet 2022 by{" "}
-        <span className="text-[#428BCA] cursor-pointer">wsi-dev</span>
+        <Link to={pathname} className="text-[#428BCA]">
+          wsi-dev
+        </Link>
       </p>
     </footer>
   );
