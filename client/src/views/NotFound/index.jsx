f50ecb613b19b63ce868a8082849490f8ef9281a
Nguyen Lu Hoang Sang <hoangsang.nguyen2001a@gmail.com>
2023-01-09 09:28:09 +0700

    update not found page

--- client/src/views/NotFound/index.jsx
+++ client/src/views/NotFound/index.jsx
@@ -7,12 +7,20 @@ const NotFound = () => {
   }, []);
 
   return (
-    <div className="bg-black">
-      <h1 className="text-center text-[#222222] text-[100px] leading-none shadow-black not-found">
+    <div className="text-center bg-[#222222] -mx-[40px] -my-[20px] h-[calc(100vh-50px-60px)] flex items-center justify-center flex-col">
+      <h1 className="text-[#222222] text-[100px] leading-none not-found">
         Not Found <br /> (#404)
       </h1>
 
-      <div></div>
+      <h3 className="text-[#fff] text-[20px] mt-[30px] mx-auto mb-[30px] font-[calibri] font-bold">
+        Wibet Alert
+      </h3>
+
+      <h3 className="text-[#fff] text-[20px] mt-0 mx-auto mb-[30px] font-[calibri]">
+        Impostor detected !!!
+        <br />
+        Page not found.
+      </h3>
     </div>
   );
 };
