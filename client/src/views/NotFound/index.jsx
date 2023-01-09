d09524ea926d17084dc79f3886a0efce5f416c71
Nguyen Lu Hoang Sang <hoangsang.nguyen2001a@gmail.com>
2023-01-09 09:02:26 +0700

    update not found page

--- client/src/views/NotFound/index.jsx
+++ client/src/views/NotFound/index.jsx
@@ -1,7 +1,20 @@
-import React from "react";
+import React, { useEffect } from "react";
 
 const NotFound = () => {
-  return <div>NotFound</div>;
+  // Set title
+  useEffect(() => {
+    document.title = "Not Found (#404)";
+  }, []);
+
+  return (
+    <div className="bg-black">
+      <h1 className="text-center text-[#222222] text-[100px] leading-none shadow-black not-found">
+        Not Found <br /> (#404)
+      </h1>
+
+      <div></div>
+    </div>
+  );
 };
 
 export default NotFound;
