fc24817529094d5d3202d7b8dce1f630db5a35db
Nguyen Lu Hoang Sang <hoangsang.nguyen2001a@gmail.com>
2023-02-21 10:28:26 +0700

    update new styles for champion team

--- client/src/views/Brackets/index.jsx
+++ client/src/views/Brackets/index.jsx
@@ -609,7 +609,9 @@ const Brackets = () => {
       </div>
 
       <div className="ml-16 flex flex-col items-center justify-center">
-        <p>{game[14]?.result || "Champion team"}</p>
+        <p className="text-[28px] text-[#333333]">
+          {game[14]?.result || "Champion team"}
+        </p>
         <p className="font-[calibri] text-[12px] text-[#999999]">Champion</p>
       </div>
     </div>
